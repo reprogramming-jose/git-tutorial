28ed616a34e540253f375a2f590e9ff2e188cb5e
Jose <reprogramming.jose@outlook.com>
2023-10-08 18:16:42 -0400

    last commit

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 console.log('config.js');
-<<<<<<< HEAD
-console.log('version14');
-=======
 console.log('version15');
->>>>>>> bd823d69261063716a63357ff54f3580441a5975
 
 s
--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 console.log('index.js');
-<<<<<<< HEAD
-console.log('version14');
-=======
 console.log('version15');
->>>>>>> bd823d69261063716a63357ff54f3580441a5975
